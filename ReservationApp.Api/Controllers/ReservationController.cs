b7dbed8e4edd637a32554ac0349426308491b24b
Emir Büçkün <emirbuckun@hotmail.com>
2023-07-27 18:25:19 +0300

    Controller methods modified, validation added

--- ReservationApp.Api/Controllers/ReservationController.cs
+++ ReservationApp.Api/Controllers/ReservationController.cs
@@ -2,6 +2,7 @@
 using ReservationApp.Api.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.IO.Compression;
 
 namespace ReservationApp.Api.Controllers
 {
@@ -19,7 +20,7 @@ namespace ReservationApp.Api.Controllers
         }
 
         [HttpGet]
-        [Route("list")]
+        [Route("getAll")]
         public async Task<List<Reservation>> Get()
         {
             return await _set.ToListAsync();
@@ -33,35 +34,90 @@ namespace ReservationApp.Api.Controllers
             return entity;
         }
 
-        [HttpPost]
-        public async Task<Reservation> Add(Reservation request)
+        [HttpGet]
+        [Route("getByName")]
+        public async Task<List<Reservation>?> Get(string name)
         {
-            request.CreatedAt = DateTime.Now;
-            _set.Add(request);
-            await _context.SaveChangesAsync();
-            return request;
+            var entity = await _set.Where(x => EF.Functions.Like(x.Name, "%" + name + "%")).ToListAsync();
+            return entity;
+        }
+
+        [HttpGet]
+        [Route("getByDateRange")]
+        public async Task<List<Reservation>?> Get(DateTime startDate, DateTime endDate)
+        {
+            if (startDate > endDate) return null;
+            var entity = await _set.Where(x => x.ReservationDate.Date >= startDate.Date && x.ReservationDate.Date <= endDate.Date).ToListAsync();
+            return entity;
+        }
+
+        [HttpPost]
+        public async Task<string> Add(Reservation request)
+        {
+            var validation = IsValid(request).Result;
+            if (validation.IsSuccess)
+            {
+                request.CreatedAt = DateTime.Now;
+                _set.Add(request);
+                await _context.SaveChangesAsync();
+                return "Reservation created successfully!";
+            }
+            else return validation.Message;
         }
 
         [HttpPut]
-        public async Task<Reservation> Update(Reservation request)
+        public async Task<string> Update(Reservation request)
         {
-            request.UpdatedAt = DateTime.Now;
-            _set.Update(request);
-            await _context.SaveChangesAsync();
-            return request;
+            var validation = IsValid(request).Result;
+            if (validation.IsSuccess)
+            {
+                request.UpdatedAt = DateTime.Now;
+                _set.Update(request);
+                await _context.SaveChangesAsync();
+                return "Reservation updated successfully!";
+            }
+            else return validation.Message;
         }
 
         [HttpDelete]
-        public async Task<bool> Delete(int id)
+        public async Task<string> Delete(int id)
         {
             var entity = await Get(id);
             if (entity != null)
             {
                 _set.Remove(entity);
                 await _context.SaveChangesAsync();
-                return true;
+                return "Reservation deleted successfully!";
             }
-            return false;
+            return "No reservation found!";
+        }
+
+        private async Task<Response> IsValid(Reservation request)
+        {
+            var id = request.Id;
+            var startHour = request.StartHour;
+            var endHour = request.StartHour + request.Duration;
+            var date = request.ReservationDate;
+            var dayOfWeek = date.DayOfWeek.ToString();
+            var duration = request.Duration;
+
+            // Out of working hours
+            if (startHour < 9 || endHour > 17 || dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+                return new Response(false, "Date range is out of working hours!");
+
+            // Max 4 - Min 1 hours duration
+            if (duration < 1 || duration > 4)
+                return new Response(false, "Duration should be between 1 and 4 hours!");
+
+            // Already booked
+            var entity = await _set.SingleOrDefaultAsync(x => x.Id != id && x.ReservationDate.Date == date.Date && // Diff Record && Same Day
+                        ((x.StartHour >= startHour && x.StartHour < endHour) ||  // Start hour of the record btw request start and end hour
+                        ((x.StartHour + x.Duration) > startHour && (x.StartHour + x.Duration) <= endHour))); // End hour of the record btw request start and end hour
+
+            if (entity != null) return new Response(false, "This date range is already booked!");
+
+            // Valid
+            return new Response(true, "Success");
         }
     }
 }
